c5ad36a70b8575b240d820583bf089a1776eef5c
Alberto Quero Brunete <alberto.quero@civica-soft.com>
2024-10-27 15:01:14 +0000

    subida de todo

---
+++ models/staging/google_sheets/stg_google_sheets__budget_incr.sql
@@ -0,0 +1,28 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+WITH src_budget AS (
+    SELECT * 
+    FROM {{ source('google_sheets', 'budget') }}
+    ),
+
+renamed_casted AS (
+    SELECT
+          _row
+        , product_id
+        , quantity
+        , month
+        , _fivetran_synced AS date_load
+    FROM src_budget
+    )
+
+SELECT * FROM renamed_casted
+
+{% if is_incremental() %}
+
+  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+
+{% endif %}
--- models/staging/sql_server_dbo/stg_sql_server_dbo__users.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__users.sql
@@ -15,7 +15,7 @@ renamed as (
         phone_number,
         total_orders,
         first_name,
-        email,
+        email as correo,
         _fivetran_deleted,
         _fivetran_synced as date_load
 
